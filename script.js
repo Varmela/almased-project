a8cca5248d2fd5285794697e8bf5d172f92191df
Varmela <varmela.basha@gmail.com>
2025-06-26 10:49:12 +0200

    header mobile & banner

---
+++ script.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const toggleButton = document.querySelector('.menu-toggle');
+  const LinksList  = document.querySelector('.links');
+
+  if (!toggleButton || !LinksList) return;
+
+  //open or close when the menu is clicked
+  toggleButton.addEventListener('click', (e) => {
+    e.stopPropagation();
+    LinksList.classList.toggle('open');
+  });
+
+  // Close the menu when clicking outside of it
+  document.addEventListener('click', (e) => {
+    if (!LinksList.contains(e.target) && !toggleButton.contains(e.target)) {
+      LinksList.classList.remove('open');
+    }
+  });
+});
