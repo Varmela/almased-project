83798fad06536d2d893b7ce717d0b193f6dc5a26
Varmela <varmela.basha@gmail.com>
2025-06-30 12:51:46 +0200

    Testimonials-without-swiper

--- script.js
+++ script.js
@@ -17,3 +17,22 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   });
 });
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'vertical',
+  loop: true,
+
+
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
